14c8cbad916edef2a59f5ad18a9bbf0956dedd20
Steve Sutton <steve.sutton@unipro.io>
2023-02-23 13:16:18 +0000

    Remove using

--- SpecFlowProject1/StepDefinitions/GameOfLifeRulesStepDefinitions.cs
+++ SpecFlowProject1/StepDefinitions/GameOfLifeRulesStepDefinitions.cs
@@ -1,5 +1,3 @@
-using System.Drawing;
-
 namespace SpecFlowProject1.StepDefinitions
 {
     [Binding]
--- SpecFlowProject1/StepDefinitions/TestGrid.cs
+++ SpecFlowProject1/StepDefinitions/TestGrid.cs
@@ -9,7 +9,7 @@ namespace SpecFlowProject1.StepDefinitions
         private static readonly int[] neighbourOffsets = { 0, 1, 2, 3, 5, 6, 7, 8 };
 
         public static Grid BuildWithNeighbours(bool status, int neighbours)
-        {            
+        {
             var grid = new Grid(size, Generator.Default(size));
             for (int i = 0; i < neighbours; i++) grid[neighbourOffsets[i]] = true;
             grid[Centre] = status;
