d9101b153697fc7b0ff8ec611d8703fffb2ad311
Steve Sutton <steve.sutton@unipro.io>
2023-02-23 13:15:10 +0000

    Improve test steps using TestGrid

--- SpecFlowProject1/StepDefinitions/GameOfLifeRulesStepDefinitions.cs
+++ SpecFlowProject1/StepDefinitions/GameOfLifeRulesStepDefinitions.cs
@@ -5,21 +5,12 @@ namespace SpecFlowProject1.StepDefinitions
     [Binding]
     public class GameOfLifeRulesStepDefinitions : GridScenarioContext
     {
-        private readonly int centre = 4;
-        private readonly int[] neighbourOffsets = { 0, 1, 2, 3, 5, 6, 7, 8 };
-
         public GameOfLifeRulesStepDefinitions(ScenarioContext scenarioContext) : base(scenarioContext) { }
 
         [Given(@"a (.*) cell with (.*) neighbours")]
         public void GivenAStatusCellWithNeighoursNeighbours(string status, int neighbours)
         {
-            var size = new Size(3, 3);
-            Grid = new Grid(size, Generator.Default(size));
-            for (int i = 0; i < neighbours; i++)
-            {
-                Grid[neighbourOffsets[i]] = true;
-            }
-            Grid[centre] = status == "live";
+            Grid = TestGrid.BuildWithNeighbours(status == "live", neighbours);
         }
 
         [When(@"the next generation is calculated")]
@@ -31,7 +22,7 @@ namespace SpecFlowProject1.StepDefinitions
         [Then(@"the cell is (.*)")]
         public void ThenTheCellIsNow(string now)
         {
-            Grid[centre].Should().Be(now == "live");
+            Grid[TestGrid.Centre].Should().Be(now == "live");
         }
     }
 }
---
+++ SpecFlowProject1/StepDefinitions/TestGrid.cs
@@ -0,0 +1,19 @@
+using System.Drawing;
+
+namespace SpecFlowProject1.StepDefinitions
+{
+    public static class TestGrid
+    {
+        private static Size size = new(3, 3);
+        public static readonly int Centre = 4;
+        private static readonly int[] neighbourOffsets = { 0, 1, 2, 3, 5, 6, 7, 8 };
+
+        public static Grid BuildWithNeighbours(bool status, int neighbours)
+        {            
+            var grid = new Grid(size, Generator.Default(size));
+            for (int i = 0; i < neighbours; i++) grid[neighbourOffsets[i]] = true;
+            grid[Centre] = status;
+            return grid;
+        }
+    }
+}
